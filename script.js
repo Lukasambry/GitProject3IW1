e624704306b53a100d315ff08c95642f466cc340
AZARKANE Issam <issam.azarkane@gmail.com>
2023-12-13 15:07:29 +0100

    Starter

---
+++ script.js
@@ -0,0 +1,36 @@
+function gestionnaireDeTaches() {
+    let taches = ["faire les courses", "nettoyer la maison", "promener le chien"];
+    let tacheComplete = false;
+
+    function ajouterTache(tache) {
+        if (tache != null) {
+            taches.push(tache);
+        }
+    }
+
+    function supprimerTache(index) {
+        if (index > 0 && index <= taches.length) {
+            taches.splice(index, 1);
+        }
+    }
+
+    function afficherTaches() {
+        for (i = 0; i < taches.length; i++) {
+            console.log(i + " - " + taches[i]);
+        }
+    }
+
+    function marquerTacheComplete(index) {
+        if (index > 0 && index <= taches.length) {
+            tacheComplete[index] = true;
+        }
+    }
+
+    ajouterTache("lire un livre");
+    supprimerTache(2);
+    afficherTaches();
+    marquerTacheComplete(1);
+    afficherTaches();
+}
+
+gestionnaireDeTaches();
